faf48436c9e45f5721ee70fddf74fa2cb1752c4d
emilianomassolin <e.massolin@alumno.um.edu.ar>
2025-04-21 16:47:35 -0300

    agrego libro a recursos

--- src/modelo/recurso/RecursoDigital.java
+++ src/modelo/recurso/RecursoDigital.java
@@ -1,9 +1,8 @@
 package modelo.recurso;
 
 public interface RecursoDigital {
-    String getId();
+
     String getTitulo();
-    EstadoRecurso getEstado();
-    void setEstado(EstadoRecurso estado);
     String getDescripcion();
+    String getAutor();
 }
