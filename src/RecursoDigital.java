fc1fd35f9e6f7557824ac0470b67192a04358b44
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2025-04-22 18:58:25 +0000

    docs:mejora-de-consola

--- src/RecursoDigital.java
+++ src/RecursoDigital.java
@@ -2,7 +2,6 @@ package src;
 
 public interface RecursoDigital {
     String getTitulo();
-    String getId(); // Changed to getId()
-    EstadoRecurso getEstado();
-    void actualizarEstado(EstadoRecurso estado);
+    String getId();
+    void mostrarDetalles();
 }
